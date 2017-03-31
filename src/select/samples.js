bdbc020ae0bfbad372a2802ebd35e085920d68d9
Kai Curry <kai@cape.io>
2017-03-31 10:09:48 -0400

    fields schema

--- src/select/samples.js
+++ src/select/samples.js
@@ -6,13 +6,41 @@ import { patternIndex } from './home'
 
 export const samplePatterns = flow(getDb('sampleOrder'), keys)
 
+// Confirmation of a transaction, contains multiple line items,
+// each represented by an Offer that has been accepted by the customer.
+export const ORDER = 'Order' // Might just use a list instead?
 export const items = createSelector(samplePatterns, patternIndex, (ids, patterns) =>
   (patterns && ids && patterns[ids[0]] && map(ids, propertyOf(patterns))) || null
 )
+
+export const name = {
+  id: 'name',
+  // description: 'Full Name',
+  position: 0,
+  isRequired: true,
+  name: 'Full Name',
+  type: 'text',
+  validators: ['isRequired'],
+}
+export const company = {
+  id: 'company',
+  // description: 'Company',
+  position: 1,
+  isRequired: true,
+  name: 'Company',
+  type: 'text',
+  validators: ['isRequired'],
+}
+export const orderFields = {
+  name,
+  company,
+}
 
 const sampleSelector = createStructuredSelector({
   items,
   patternIndex,
   samplePatterns,
+  orderFields,
+  entityType: ORDER,
 })
 export default sampleSelector
